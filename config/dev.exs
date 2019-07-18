6fab77d1262cdd7d7b1bc6590a5c4da37dca48dc
Basile Nouvellet <basile.nouvellet@ponts.org>
2019-07-18 09:49:44 +0200

    Add start script

--- config/dev.exs
+++ config/dev.exs
@@ -66,11 +66,11 @@ config :phoenix_live_view_demo, PhoenixLiveViewDemoWeb.Endpoint,
   ]
 
 # Do not include metadata nor timestamps in development logs
-config :logger, :console, format: "[$level] $message\n"
+config :logger, :console, format: "\n\n\n[$level]\n $message\n\n"
 
 # Set a higher stacktrace during development. Avoid configuring such
 # in production as building large stacktraces may be expensive.
-config :phoenix, :stacktrace_depth, 20
+config :phoenix, :stacktrace_depth, 30
 
 # Initialize plugs at runtime for faster development compilation
 config :phoenix, :plug_init_mode, :runtime
